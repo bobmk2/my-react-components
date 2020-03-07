11859a6b0bd93dd1f4f4d458e66731f1a641c26c
bobmk2 <booby.bob.mk2@gmail.com>
2020-03-07 20:48:25 +0900

    Remove config.js setting file

---
+++ .storybook/preview.js
@@ -0,0 +1,5 @@
+import { addDecorator } from '@storybook/react';
+import '@storybook/addon-console';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+addDecorator(withInfo, withKnobs);
